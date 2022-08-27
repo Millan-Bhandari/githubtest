f46221f2f998ff839e67515c6bdfb430015684f2
Millan Bhandari <millan.ib@icloud.com>
2022-08-27 12:50:49 -0500

    Initial Commit

---
+++ githubtest/githubtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubtestApp.swift
+//  githubtest
+//
+//  Created by Millan Bhandari on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct githubtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
